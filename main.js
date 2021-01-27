b2f57a22f0782d87ccf30bc29243e48859f624b7
Jamila <jaja_mimi@hotmail.com>
2021-01-27 09:30:39 +0100

    fin

---
+++ main.js
@@ -0,0 +1,16 @@
+// 1 
+let tartine = document.getElementsByClassName('tartine')[0];
+
+console.log(tartine.innerHTML);
+
+//2
+
+let toast = document.getElementById('toast')
+
+console.log(toast.innerText);
+
+//3 
+
+let lastTartine = document.getElementsByClassName('tartine')[1]
+
+console.log(lastTartine);
